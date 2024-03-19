e76ffc45b2454248723013bdad369e790c58e656
Joey Akitoye <josephakitoye@gmail.com>
2024-03-19 08:43:51 +0000

    Randomize datapoints
    Signed-off-by: Joey Akitoye <josephakitoye@gmail.com>

--- main.go
+++ main.go
@@ -7,18 +7,18 @@ import (
 
 func main() {
 	client := rimbus.New()
-	client.EstablishConnection() // Only for RabbitMQ
+	client.EstablishConnection() // Only necessary for RabbitMQ
 	event := rimbus.NewEvent(rimbus.ProductApi, rimbus.InventoryCreateEvent, rimbus.ProductCreateEvent)
 	event.Payload = map[string]any{
 		"product": map[string]any{
 			"id":           "bb4ef24b-1699-4452-ad09-f284e57c6049",
-			"barcode":      "00022233444",
+			"barcode":      "1234567890",
 			"name":         "Apple iPhone Charger",
 			"description":  "Apple iPhone Charger Description",
-			"supply_price": 800,
-			"retail_price": 1000,
+			"supply_price": 800.40,
+			"retail_price": 1000.85,
 			"type":         "product",
-			"amount":       1200,
+			"amount":       1200.0,
 			"category_id":  "123",
 			"company_id":   "3ec34288-5ce8-4974-b05e-6e50a32465bb",
 		},
